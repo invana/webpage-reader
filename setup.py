b12530c17e8b765e64f98079e21970629b56385c
Ravi RT Merugu <rrmerugu@gmail.com>
2018-09-18 03:34:15 +0530

    requirements updated

--- setup.py
+++ setup.py
@@ -6,15 +6,19 @@ with open('requirements.txt', 'r') as f:
             line.strip(' \n') for line in f
         ] if not s.startswith('#') and s != ''
     ]
-    
+
 setup(
     name='webpage-reader',
-    version='0.0.3',
+    version='0.0.4',
     packages=['webpage_reader'],
     url='https://github.com/invanatech/webpage-reader',
     license='MIT License',
     author='rrmerugu',
     author_email='rrmerugu@gmail.com',
     description='Reads a webpage and extracts the information out of it, based on the HTML5 tags/classes ',
-    install_requires=install_reqs
+    install_requires=[
+        "requests==2.18.4",
+        "beautifulsoup4==4.6.0",
+        "flatten_json==0.1.6"
+    ]
 )
